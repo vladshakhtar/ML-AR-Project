790a61e03293ee4d33988e2be80b9054afd777c1
vladshakhtar <stolyarovs2003@gmail.com>
2023-07-03 11:41:24 +0300

    Final version!

--- Recipe FinderUITests/UITestingRecentView_UITests.swift
+++ Recipe FinderUITests/UITestingRecentView_UITests.swift
@@ -1,13 +1,13 @@
 //
-//  UITestingFavouritesView_UITests.swift
+//  UITestingRecentView_UITests.swift
 //  Recipe FinderUITests
 //
-//  Created by Vladislav Stolyarov on 30.06.2023.
+//  Created by Vladislav Stolyarov on 03.07.2023.
 //
 
 import XCTest
 
-final class UITestingFavouritesView_UITests: XCTestCase {
+final class UITestingRecentView_UITests: XCTestCase {
     let app = XCUIApplication()
     
     override func setUpWithError() throws {
@@ -18,20 +18,20 @@ final class UITestingFavouritesView_UITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func test_FavouritesView_DefaultSettings() {
+    
+    func test_RecentView_DefaultSettings() {
         
         //MARK: GIVEN
-        let favouritesViewTab = app.tabBars["Tab Bar"].buttons["Favorites"]
-        let favouritesNavigationBar = app.navigationBars["Favourites"]
-        let defaultSortByButton = favouritesNavigationBar/*@START_MENU_TOKEN@*/.buttons["Sort by:, Added Time"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Sort by:, Added Time\"]",".buttons[\"Sort by:, Added Time\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        let defaultSortingOrderButton = favouritesNavigationBar/*@START_MENU_TOKEN@*/.buttons["Newest"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Newest\"]",".buttons[\"Newest\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let recentViewTab = app.tabBars["Tab Bar"].buttons["Recent"]
+        let recentNavigationBar = app.navigationBars["Recent"]
+        let defaultSortByButton = recentNavigationBar/*@START_MENU_TOKEN@*/.buttons["Sort by:, Added Time"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Sort by:, Added Time\"]",".buttons[\"Sort by:, Added Time\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let defaultSortingOrderButton = recentNavigationBar/*@START_MENU_TOKEN@*/.buttons["Newest"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Newest\"]",".buttons[\"Newest\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         
         //MARK: WHEN
-        favouritesViewTab.tap()
+        recentViewTab.tap()
         
         //MARK: THEN
-        XCTAssertTrue(favouritesNavigationBar.exists)
+        XCTAssertTrue(recentNavigationBar.exists)
         XCTAssertTrue(defaultSortByButton.exists)
         XCTAssertTrue(defaultSortingOrderButton.exists)
         
@@ -40,9 +40,9 @@ final class UITestingFavouritesView_UITests: XCTestCase {
     func test_FavouritesViewActionShett_ShowsInfo() {
         
         //MARK: GIVEN
-        let favouritesViewTab = app.tabBars["Tab Bar"].buttons["Favorites"]
-        let favouritesNavigationBar = app.navigationBars["Favourites"]
-        let defaultSortByButton = favouritesNavigationBar/*@START_MENU_TOKEN@*/.buttons["Sort by:, Added Time"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Sort by:, Added Time\"]",".buttons[\"Sort by:, Added Time\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let recentViewTab = app.tabBars["Tab Bar"].buttons["Recent"]
+        let recentNavigationBar = app.navigationBars["Recent"]
+        let defaultSortByButton = recentNavigationBar/*@START_MENU_TOKEN@*/.buttons["Sort by:, Added Time"]/*[[".otherElements[\"Sort by:, Added Time\"].buttons[\"Sort by:, Added Time\"]",".buttons[\"Sort by:, Added Time\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         let actionSheetCancelButton = app/*@START_MENU_TOKEN@*/.otherElements["Sort Recipes By:"].scrollViews/*[[".otherElements[\"Sort Recipes By:\"].scrollViews",".scrollViews"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.otherElements.buttons["Cancel"]
         let actionSheetAddedTimeButton = app/*@START_MENU_TOKEN@*/.otherElements["Sort Recipes By:"].scrollViews/*[[".otherElements[\"Sort Recipes By:\"].scrollViews",".scrollViews"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.otherElements.buttons["Added Time"]
         let actionSheetNameButton = app.otherElements["Sort Recipes By:"].scrollViews.otherElements.buttons["Name"]
@@ -52,7 +52,7 @@ final class UITestingFavouritesView_UITests: XCTestCase {
         let actionSheetProteinButton = app/*@START_MENU_TOKEN@*/.otherElements["Sort Recipes By:"].scrollViews/*[[".otherElements[\"Sort Recipes By:\"].scrollViews",".scrollViews"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/.otherElements.buttons["Protein"]
         
         //MARK: WHEN
-        favouritesViewTab.tap()
+        recentViewTab.tap()
         defaultSortByButton.tap()
         
         //MARK: THEN
@@ -64,6 +64,6 @@ final class UITestingFavouritesView_UITests: XCTestCase {
         XCTAssertTrue(actionSheetCarbsButton.exists)
         XCTAssertTrue(actionSheetProteinButton.exists)
     }
-    
-    
+
+
 }
